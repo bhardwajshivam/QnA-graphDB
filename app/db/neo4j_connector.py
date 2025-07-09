f21155dea63985787373bc28065ab53e1d063051
Shivam Bhardwaj <bhardwajshivam121@gmail.com>
2025-07-08 21:03:54 -0400

    created folder structure

---
+++ app/db/neo4j_connector.py
@@ -0,0 +1,9 @@
+import getpass
+import os
+
+if "OPENAI_API_KEY" not in os.environ:
+    os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter your OpenAI API key: ")
+
+os.environ["NEO4J_URI"] = "bolt://localhost:7687"
+os.environ["NEO4J_USERNAME"] = "neo4j"
+os.environ["NEO4J_PASSWORD"] = "password"
